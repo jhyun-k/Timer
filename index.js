ddc800b5a83f0f3c90ad91757a570c855564cfd6
jhyun-k <jhkim1238@naver.com>
2022-11-10 01:22:57 +0900

    js 기능 완

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const resetBtn = document.querySelector('.reset')
 const startImg = document.querySelector('.startImg')
 const resetImg = document.querySelector('.resetImg')
 
+let interval
+
+/* 인풋값에 초 넣을때 한자리로 보이는거 어떻게 해결하지? */
 
 /* 버튼 보이기 숨기기 */
 function showBtn(btn){
@@ -32,25 +35,58 @@ for(i of input){
 
 }
 
+/* 총 초수 받아서 setInterval 하기, 시간 끝나면 버튼 다시 비활성화 되어야함 */
 
-function timer (){    
-    const interval = setInterval(() => {
-        sec.value = sec.value-=1
-        console.log(sec.value)
-        if(sec.value<=0){
-            sec.value='00'
+function startTimer(totalSeconds){
+    interval = setInterval(()=>{
+        totalSeconds--;
+        updateInputs(totalSeconds)
+        if(totalSeconds<=0){
+             sec.value='00'
             clearInterval(interval)
-        } 
-    }, 1000)
+            alert('지정한 시간이 끝났습니다')
+            showBtn(startBtn)
+            hideBtn(pauseBtn)
+            startBtn.classList.remove('able');
+            resetBtn.classList.remove('able');
+        }
+    },1000)
 }
 
+/* input값 업데이트, 한자리수일 경우에는 앞에 0 붙이기 */
+function updateInputs(totalSeconds){
+    const hours = Math.floor(totalSeconds / 60 /60);
+    const minutes = Math.floor(totalSeconds/60)
+    const seconds = totalSeconds % 60
+
+    hrs.value = hours
+    min.value = minutes
+    sec.value = seconds
+
+    if(hours<10){
+        hrs.value='0'+ hours
+    }
+    if(minutes<10){
+        min.value='0' + minutes
+    }
+    if(seconds<10){
+        sec.value='0' + seconds
+    }
+}
+
+/* 각 버튼에 이벤트 넣기 */
+
 startBtn.addEventListener('click',()=>{
+    const hours = parseInt(hrs.value)
+    const minutes = parseInt(min.value)
+    const seconds = parseInt(sec.value)
+
+    const totalSeconds = (hours * 60 * 60) + (minutes * 60) + seconds
+    startTimer(totalSeconds)
     showBtn(pauseBtn)
     hideBtn(startBtn)
-    timer()
 
 })
-
 
 resetBtn.addEventListener('click',()=>{
     hrs.value='00';
@@ -65,3 +101,17 @@ pauseBtn.addEventListener('click',()=>{
     hideBtn(pauseBtn)
     clearInterval(interval)
 })
+
+
+
+
+// function timer (){    
+//     const interval = setInterval(() => {
+//         sec.value = sec.value-=1
+//         console.log(sec.value)
+//         if(sec.value<=0){
+//             sec.value='00'
+//             clearInterval(interval)
+//         } 
+//     }, 1000)
+// }
